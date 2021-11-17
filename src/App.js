69a6d5c59b9d94598141d7607c29334b03eb2826
Kaio <luizkaio9815@gmail.com>
2021-11-17 14:03:45 -0300

    Cria a aplicação reserva do projeto final do Start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './styles/App.css'
+import { Header } from './components/Header';
+import { Rotas } from './Routes';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Rotas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Criadores.js
@@ -0,0 +1,94 @@
+import React, { useEffect } from "react";
+// import { Perfil } from "../components/Perfil";
+import '../styles/Criadores.css'
+
+import Emanuel from "../assets/Perfil/isack.png"
+import Kaio from "../assets/Perfil/kaio.png"
+import Marcelo from "../assets/Perfil/marcelo.png"
+import Nara from "../assets/Perfil/nara.png"
+import Michele from "../assets/Perfil/michele.png"
+import Git from "../assets/Perfil/Git.png"
+import Linkedin from "../assets/Perfil/Linkedin.png"
+import Thassio from "../assets/Perfil/thassio.jpg"
+
+export const Criadores =()=> {
+
+    useEffect(()=>{
+        document.title = 'Rick & Morty - Criadores'
+    }, []);
+    
+   
+  return (
+    <section>
+      <div className="container-criadores">
+          
+        <Card
+            imagem={Emanuel}
+            title="EMANUEL" 
+            imgGit={Git}
+            imgLinkedin={Linkedin}
+            linkGit="https://github.com/manelxd"
+            linkedin="https://www.linkedin.com/in/isaac-emanuel-3167a5219/"
+        />
+
+        <Card
+            imagem={Marcelo}
+            title="MARCELO"
+            imgGit={Git}
+            imgLinkedin={Linkedin}
+            linkGit="https://github.com/MarceloSilva05"
+            linkedin="https://www.linkedin.com/in/marcelo-silva05"
+        />
+
+        <Card
+            imagem={Thassio}
+            title="THASSIO"
+            imgGit={Git}
+            imgLinkedin={Linkedin}
+            linkGit="https://github.com/Thassio98"
+            linkedin="https://www.linkedin.com/in/thassio-ferreira-2947b6162/"
+        />
+
+        <Card
+            imagem={Nara}
+            title="NARA"
+            imgGit={Git}
+            imgLinkedin={Linkedin}
+            linkGit="https://github.com/inaraoliveira"
+            linkedin="https://www.linkedin.com/in/inara-oliveira/"
+        />
+        <Card
+            imagem={Kaio}
+            title="KAIO"
+            imgGit={Git}
+            imgLinkedin={Linkedin}
+            linkGit="https://github.com/kaioluiz03"
+            linkedin="https://www.linkedin.com/in/kaioluiz03/" 
+        />
+        <Card
+            imagem={Michele}
+            title="MICHELE"
+            imgGit={Git}
+            imgLinkedin={Linkedin}
+            linkGit="https://github.com/miihfranca"
+            linkedin="https://www.linkedin.com/in/michele-fran%C3%A7a-45598518a/"  
+        />
+      </div>
+    </section>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card_perfil">
+      <img src={props.imagem} className="card_imagem"alt="Imagem de perfil"/>
+      <div className="card_body">
+        <h2 className="card_title">{props.title}</h2>
+      </div>
+      <div className="icon" > 
+        <a href={props.linkGit} target='_blank' rel="noreferrer"><img className="git" src={props.imgGit}alt="logo git"/></a>
+        <a href={props.linkedin} target='_blank' rel="noreferrer"><img className="linkedin" src={props.imgLinkedin}alt="logo linkedin"/></a>
+      </div>
+    </div>
+  );
+}
